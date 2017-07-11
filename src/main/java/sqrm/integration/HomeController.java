3af70394c0a9b71eee05ca4e4861544c89be2c2f
mikhail <mikhail@mikhail-GP72-6QF>
2017-07-11 13:45:46 +0300

    Initial commit

---
+++ src/main/java/sqrm/integration/HomeController.java
@@ -0,0 +1,13 @@
+package sqrm.integration;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/home")
+	public String hello() {
+		return "Stat will be here";
+	}
+}
---
+++ src/main/java/sqrm/integration/SqrmIntegrationApplication.java
@@ -0,0 +1,12 @@
+package sqrm.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqrmIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqrmIntegrationApplication.class, args);
+	}
+}
